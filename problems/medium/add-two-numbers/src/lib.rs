39027055b4f5ad7c1b03f260ad3c9599d9c57ee8
Michael Hancock <220162+gringasalpastor@users.noreply.github.com>
2020-12-06 16:49:53 -0500

    update fmt to use Optional directly, remove calls to unwrap, and move reverse function to node.rs

--- problems/medium/add-two-numbers/src/lib.rs
+++ problems/medium/add-two-numbers/src/lib.rs
@@ -2,21 +2,6 @@ mod node;
 
 use node::*;
 
-fn reverse(list: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-    let mut previous = None;
-    let mut current = list;
-
-    while let Some(ref mut cur) = current {
-        let next = cur.next.take();
-        cur.next = previous;
-
-        previous = current;
-        current = next;
-    }
-
-    return previous;
-}
-
 pub struct Solution;
 
 impl Solution {
