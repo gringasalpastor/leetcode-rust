fecc5b93fd0c405c9029e1c8ad4e903941fb8e19
Michael Hancock <220162+gringasalpastor@users.noreply.github.com>
2020-12-12 10:56:11 -0500

    clean-up

--- problems/medium/add-two-numbers/src/lib.rs
+++ problems/medium/add-two-numbers/src/lib.rs
@@ -1,4 +1,4 @@
-mod node;
+pub mod node;
 
 use node::*;
 
@@ -47,7 +47,7 @@ mod tests {
     fn basics() {
         let empty = ListNode::from_slice(&[]);
         let ex123 = ListNode::from_slice(&[1, 2, 3]);
-        let ex123_rev = reverse(ListNode::from_slice(&[1, 2, 3]));
+        let ex321 = ListNode::from_slice(&[3, 2, 1]);
 
         assert_eq!(ex123_rev.wrap().to_string(), "3, 2, 1");
         assert_eq!(ex123.wrap().to_string(), "1, 2, 3");
@@ -57,6 +57,22 @@ mod tests {
                 .wrap()
                 .to_string(),
             "4, 4, 4"
+        );
+
+        let ex243 = ListNode::from_slice(&[2, 4, 3]);
+        let ex564 = ListNode::from_slice(&[5, 6, 4]);
+        assert_eq!(
+            Solution::add_two_numbers(ex243, ex564).wrap().to_string(),
+            "7, 0, 8"
+        );
+
+        let ex9999999 = ListNode::from_slice(&[9, 9, 9, 9, 9, 9, 9]);
+        let ex9999 = ListNode::from_slice(&[9, 9, 9, 9]);
+        assert_eq!(
+            Solution::add_two_numbers(ex9999999, ex9999)
+                .wrap()
+                .to_string(),
+            "8, 9, 9, 9, 0, 0, 0, 1"
         );
     }
 }
--- problems/medium/add-two-numbers/src/node.rs
+++ problems/medium/add-two-numbers/src/node.rs
@@ -59,18 +59,3 @@ impl<'a> fmt::Display for ListNodeWrapper<'a> {
         return Ok(()); // Errors checked above via `?` operator
     }
 }
-
-pub fn reverse(list: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-    let mut previous = None;
-    let mut current = list;
-
-    while let Some(ref mut cur) = current {
-        let next = cur.next.take();
-        cur.next = previous;
-
-        previous = current;
-        current = next;
-    }
-
-    return previous;
-}
