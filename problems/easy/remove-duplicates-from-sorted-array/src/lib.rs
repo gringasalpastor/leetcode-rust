7ade99844c7bb527448d9997b93c44677dd22625
Michael Hancock <220162+gringasalpastor@users.noreply.github.com>
2021-01-01 15:30:06 -0500

    remove-duplicates-from-sorted-array

---
+++ problems/easy/remove-duplicates-from-sorted-array/src/lib.rs
@@ -0,0 +1,57 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        if nums.is_empty() {
+            return 0;
+        }
+
+        let mut insert_pos = 0;
+
+        for current_pos in 1..nums.len() {
+            if nums[insert_pos as usize] != nums[current_pos as usize] {
+                insert_pos = insert_pos + 1;
+                nums[insert_pos as usize] = nums[current_pos as usize];
+            }
+        }
+
+        insert_pos + 1
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_works() {
+        let rd = Solution::remove_duplicates;
+
+        let mut v = vec![];
+        assert_eq!(rd(&mut v), 0);
+        assert_eq!(v, []);
+
+        let mut v = vec![1];
+        assert_eq!(rd(&mut v), 1);
+        assert_eq!(v, [1]);
+
+        let mut v = vec![1, 1];
+        assert_eq!(rd(&mut v), 1);
+        assert_eq!(v, [1, 1]);
+
+        let mut v = vec![1, 1, 1];
+        assert_eq!(rd(&mut v), 1);
+        assert_eq!(v, [1, 1, 1]);
+
+        let mut v = vec![1, 1, 1, 2];
+        assert_eq!(rd(&mut v), 2);
+        assert_eq!(v, [1, 2, 1, 2]);
+
+        let mut v = vec![1, 1, 1, 2, 2];
+        assert_eq!(rd(&mut v), 2);
+        assert_eq!(v, [1, 2, 1, 2, 2]);
+
+        let mut v = vec![1, 1, 1, 2, 2, 3];
+        assert_eq!(rd(&mut v), 3);
+        assert_eq!(v, [1, 2, 3, 2, 2, 3]);
+    }
+}
