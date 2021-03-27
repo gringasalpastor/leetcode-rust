9bff7434203e3d9d36dc178c2efd1f1c3e2e4f00
Michael Hancock <220162+gringasalpastor@users.noreply.github.com>
2021-03-27 12:00:47 -0600

    contains-duplicate

---
+++ problems/easy/contains-duplicate/src/lib.rs
@@ -0,0 +1,25 @@
+pub struct Solution;
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+        let mut seen = HashSet::new();
+        for i in nums {
+            if !seen.insert(i) {
+                return true;
+            }
+        }
+        false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn contains_duplicate() {
+        use super::*;
+
+        assert_eq!(Solution::contains_duplicate(vec![1, 2, 3]), false);
+        assert_eq!(Solution::contains_duplicate(vec![1, 2, 3, 4, 3]), true);
+    }
+}
