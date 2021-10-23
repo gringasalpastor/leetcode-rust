ef87ce0841aafd3187d50fc98571fb84ae23475b
Michael Hancock <220162+gringasalpastor@users.noreply.github.com>
2021-10-23 09:35:12 -0400

    remove-nth-from-end-of-list

--- problems/medium/remove-nth-from-end-of-list/src/lib.rs
+++ problems/medium/remove-nth-from-end-of-list/src/lib.rs
@@ -15,14 +15,18 @@ impl Solution {
 
         // If removing the first element
         if index == -1 {
-            return None;
+            return match head {
+                Some(ref mut cur) => cur.next.take(),
+                None => None,
+            };
         }
 
         let mut current = &mut head;
         let mut pos = 0;
+        let mut default_val = Box::new(ListNode::new(0));
         while let Some(ref mut cur) = current {
             if pos == index {
-                cur.next = cur.next.as_mut().unwrap_or(&mut Box::new(ListNode::new(0))).next.take();
+                cur.next = cur.next.as_mut().unwrap_or(&mut default_val).next.take();
                 break;
             }
             pos += 1;
@@ -45,5 +49,6 @@ mod tests {
         assert_eq!(rnfe(ex12345, 2).wrap().to_string(), "1, 2, 3, 5");
         assert_eq!(rnfe(f_s(&[1]), 1).wrap().to_string(), "");
         assert_eq!(rnfe(f_s(&[1, 2]), 1).wrap().to_string(), "1");
+        assert_eq!(rnfe(f_s(&[1, 2]), 2).wrap().to_string(), "2");
     }
 }
